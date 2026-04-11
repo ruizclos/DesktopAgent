968b2401128b30752ef55adfd9175f6958e11a09
ruizclos <76033265+ruizclos@users.noreply.github.com>
2026-04-11 07:07:37 -0700

    123

--- obj/Debug/net8.0-windows/MainWindow.g.cs
+++ obj/Debug/net8.0-windows/MainWindow.g.cs
@@ -92,7 +92,7 @@ namespace LocalAIAgent {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/LocalAIAgent;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LocalAIAgent;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
