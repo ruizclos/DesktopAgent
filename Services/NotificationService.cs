ec67a71d450d86c82e3813b9169abe8e1aca9ac4
ruizclos <76033265+ruizclos@users.noreply.github.com>
2026-04-10 17:22:16 -0700

    123

--- Services/NotificationService.cs
+++ Services/NotificationService.cs
@@ -1,22 +1,2 @@
-using Windows.Data.Xml.Dom;
-using Windows.UI.Notifications;
-
-namespace LocalAIAgent.Services
-{
-    public class NotificationService
-    {
-        private const string APP_ID = "LocalAIAgent";
-
-        public void ShowNotification(string title, string message)
-        {
-            var toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
-
-            var stringElements = toastXml.GetElementsByTagName("text");
-            stringElements[0].AppendChild(toastXml.CreateTextNode(title));
-            stringElements[1].AppendChild(toastXml.CreateTextNode(message));
-
-            var toast = new ToastNotification(toastXml);
-            ToastNotificationManager.CreateToastNotifier(APP_ID).Show(toast);
-        }
-    }
-}
+// NotificationService is temporarily disabled due to unsupported Windows.UI.Notifications API in WPF.
+// To enable notifications in WPF, use Microsoft.Toolkit.Uwp.Notifications and update this class accordingly.
